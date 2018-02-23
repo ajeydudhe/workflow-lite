cf0f9f0aa119bd806760f9529589beeefd8b85a2
Ajey Dudhe <ajeydudhe@gmail.com>
2018-02-23 10:04:31 +0530

    Updated copyright.

--- src/test/java/org/expedientframework/workflowlite/core/actions/AsyncAlternateCaseAction.java
+++ src/test/java/org/expedientframework/workflowlite/core/actions/AsyncAlternateCaseAction.java
@@ -1,27 +1,14 @@
-//CHECKSTYLE:OFF
 /********************************************************************
- * File Name:    AlternateCaseActivity.java
+ * File Name:    AsyncAlternateCaseAction.java
  *
  * Date Created: Aug 22, 2017
  *
  * ------------------------------------------------------------------
- * SYMANTEC: Copyright © 2017 Symantec Corporation. All rights reserved.
- * THIS SOFTWARE CONTAINS CONFIDENTIAL INFORMATION AND TRADE SECRETS OF
- * SYMANTEC CORPORATION.USE, DISCLOSURE OR REPRODUCTION IS PROHIBITED
- * WITHOUT THE PRIOR EXPRESS WRITTEN PERMISSION OF SYMANTEC CORPORATION.
- * The Licensed Software and Documentation are deemed to be commercial
- * computer software as defined in FAR 12.212 and subject to restricted
- * rights as defined in FAR Section 52.227-19 "Commercial Computer Software
- * - Restricted Rights" and DFARS 227.7202, "Rights in Commercial Computer
- * Software or Commercial Computer Software Documentation", as applicable,
- * and any successor regulations.  Any use, modification, reproduction
- * release, performance, display or disclosure of the Licensed Software
- * and Documentation by the U.S. Government shall be solely in accordance
- * with the terms of this Agreement.
+ * 
+ * Copyright @ 2017 ajeydudhe@gmail.com
+ *
  *******************************************************************/
-//CHECKSTYLE:ON
 
-// PACKAGE/IMPORTS --------------------------------------------------
 package org.expedientframework.workflowlite.core.actions;
 
 import java.util.concurrent.CompletableFuture;
